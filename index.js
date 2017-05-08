f95ff0cbc329c93d3b7d982ba774f1d139be0791
Pez Cuckow <email@pezcuckow.com>
2017-05-08 10:49:32 +0100

    ES6'ify

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Credit to:
 // https://coderwall.com/p/fhgu_q/inlining-images-with-gulp-sass
-var fs    = require('fs');
-var path  = require('path');
-var types = require('node-sass').types;
+const fs    = require('fs');
+const path  = require('path');
+const types = require('node-sass').types;
 
 module.exports = function(options) {
     options = options || {};
 
-    var base = options.base || process.cwd();
+    const base = options.base || process.cwd();
     return {
         'inline-image($file)': function(file) {
             // we want to file relative to the base
-            var relativePath = './' + file.getValue();
-            var filePath = path.resolve(base, relativePath);
+            const relativePath = './' + file.getValue();
+            const filePath = path.resolve(base, relativePath);
 
             // get the file ext
-            var ext = filePath.split('.').pop();
+            const ext = filePath.split('.').pop();
 
             // read the file
-            var data = fs.readFileSync(filePath);
+            const data = fs.readFileSync(filePath);
 
-            var buffer = new Buffer(data);
-            var str = "url('data:image/" + (ext === 'svg' ? ext + '+xml' : ext)  + ";base64," + buffer.toString('base64') + "')";
+            const buffer = new Buffer(data);
+            const str = "url('data:image/" + (ext === 'svg' ? ext + '+xml' : ext)  + ";base64," + buffer.toString('base64') + "')";
             return types.String(str);
         }
     };
